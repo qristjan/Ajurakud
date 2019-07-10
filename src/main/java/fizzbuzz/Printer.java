d1ab698278b7e6ef87d74df85d6c000a97bf82a8
Siim Raudsepp <siimraudsepp@msn.com>
2019-07-10 17:26:14 +0300

    Printer ei tohi enam mõelda

--- src/main/java/fizzbuzz/Printer.java
+++ src/main/java/fizzbuzz/Printer.java
@@ -1,9 +1,21 @@
 package fizzbuzz;
 
-public class Printer {
+import ajurakud.Ajurakud;
+
+public class Printer implements Ajurakud {
 
     public static void kirjuta(String tekst) {
         System.out.println(tekst);
     }
 
+    @Override
+    public void mõtle() throws InterruptedException
+    {
+        ära();
+    }
+
+    private void ära()
+    {
+        System.exit(1337);
+    }
 }
