2274864592d473e9c6e5ce8f679a39a473845c49
Markus Peterson <Markus Peterson>
2019-07-10 18:55:58 +0300

    cocklet

---
+++ src/main/java/best69gag/bathwater/nutlet/inyou/sucklet/NutletApplication.java
@@ -0,0 +1,12 @@
+package best69gag.bathwater.nutlet.inyou.sucklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutletApplication.class, args);
+	}
+}
