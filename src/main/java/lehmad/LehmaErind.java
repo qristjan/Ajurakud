a4665f35b67a0c899475ba1f9efe70f7518a6bc4
qristjan <kristjanps96@gmail.com>
2019-07-11 10:06:40 +0300

    m

---
+++ src/main/java/lehmad/LehmaErind.java
@@ -0,0 +1,13 @@
+package lehmad;
+
+public class LehmaErind extends Exception {
+
+    LehmaErind() {
+
+    }
+
+    LehmaErind(String s) {
+        super(s);
+    }
+
+}
---
+++ src/main/java/lehmad/Lehmakett.java
@@ -0,0 +1,11 @@
+package lehmad;
+
+public class Lehmakett {
+
+    public static void main(String[] args) throws LehmaErind {
+        Lehm ema = new Lehm("Emalehm");
+        ema.lehmaKett().lehm("Markus").lehm("Siim").lehm("Kristjan");
+        ema.kirjutaLasteNimed();
+    }
+
+}
