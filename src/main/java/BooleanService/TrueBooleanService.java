c626d3c98e4521d74444ae3c75ffba90208f202e
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2019-09-07 00:27:30 +0300

    Preds

--- src/main/java/BooleanService/TrueBooleanService.java
+++ src/main/java/BooleanService/TrueBooleanService.java
@@ -1,5 +1,7 @@
 package BooleanService;
 
+import java.util.function.Predicate;
+
 public class TrueBooleanService extends BooleanService {
 
     public static final boolean TRUE = 1 == 1 && "1".equals("1");
@@ -10,6 +12,14 @@ public class TrueBooleanService extends BooleanService {
 
     public Boolean getTrueBooleanButItsFalse() {
         return !getTrueBoolean();
+    }
+
+    public Predicate<MyBoolean> getBooleanPredicate(boolean BOOLEAN) {
+        return b -> BOOLEAN == TRUE;
+    }
+
+    public boolean getBooleanByPredicate(Predicate<MyBoolean> predBOOLEAN) {
+        return predBOOLEAN.test(new TrueBoolean());
     }
 
 }
