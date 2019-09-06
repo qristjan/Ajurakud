54397553e5a1b40c6672150067878ba40be000b0
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2019-09-06 23:33:56 +0300

    it doesnt actually process words

---
+++ src/main/java/here_be_spring_web_app/service/ApiService.java
@@ -0,0 +1,12 @@
+package here_be_spring_web_app.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ApiService {
+
+    List<String> processWords(List<String> words);
+
+}
