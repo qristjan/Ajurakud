a891453c107177b13f84a0c520195d8951273ca4
qristjan <kristjanps96@gmail.com>
2021-03-06 15:26:03 +0200

    postgresssss

---
+++ src/main/java/ajurakudroot/postgre/Pair.java
@@ -0,0 +1,13 @@
+package ajurakudroot.postgre;
+
+public class Pair {
+
+    private final String left;
+    private final String right;
+
+    public Pair(String left, String right) {
+        this.left = left;
+        this.right = right;
+    }
+
+}
